28227aaa4fe8fe41c40b34e8dddec32049bf9daf
Jacob Benny John <jacobben85@gmail.com>
2016-08-15 21:08:01 -0700

    Adding java-8 and Elasticsearch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Installing htop"
-apt-get install htop
+apt-get install -y htop
+
+echo "Installing java"
+add-apt-repository ppa:webupd8team/java -y
+apt-get update -y
+apt-get install oracle-java8-installer -y
+
+echo "Installing elasticsearch"
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.3.0.deb
+dpkg -i elasticsearch-2.3.0.deb
+update-rc.d elasticsearch defaults 95 10
+/etc/init.d/elasticsearch start

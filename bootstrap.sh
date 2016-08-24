58442441fc7b24945cd428825f6b81938fe0a39f
Jacob Benny John <jacobben85@gmail.com>
2016-08-24 10:41:41 -0700

    Adding sample json file

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,4 +18,14 @@ while [  $COUNTER -lt $COUNT ]; do
   let COUNTER=COUNTER+1
 done
 
+COUNTER2=0
+CURLURL2="http://127.0.0.1:9200/sample/part"
+COUNT2=$(less /vagrant/sample.json | jq '.Sample | length')
+while [  $COUNTER2 -lt $COUNT2 ]; do
+  CURLDATA2=$(less /vagrant/sample.json | jq '.Sample['$COUNTER2']')
+  RESPONSE2=$(curl -XPOST "$CURLURL2" -sS -H 'Content-Type: application/json' -d "$CURLDATA2")
+  echo $RESPONSE2
+  let COUNTER2=COUNTER2+1
+done
+
 echo "Setup is complete, try http://192.168.56.102:8080/ on the browser to access the application."

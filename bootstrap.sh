7a7d987cd68f3898c912dbcd7151af104f82a894
Jacob Benny John <jacobben85@gmail.com>
2016-11-01 12:05:05 -0700

    More changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Installing Java 8"
-apt-get --assume-yes update
-apt-get install -y python-software-properties debconf-utils
-add-apt-repository ppa:webupd8team/java -y
-apt-get --assume-yes update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-apt-get install -y oracle-java8-installer
-apt-get install -y oracle-java8-set-default
+#apt-get --assume-yes update
+#apt-get install -y python-software-properties debconf-utils
+#add-apt-repository ppa:webupd8team/java -y
+#apt-get --assume-yes update
+#echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+#apt-get install -y oracle-java8-installer
+#apt-get install -y oracle-java8-set-default
 
 apt-get --assume-yes update
 apt-get install -y htop
 apt-get install -y maven
 apt-get install -y nmap
 
-mkdir -p /home/vagrant/.m2
-cp /vagrant/scripts/settings.xml /home/vagrant/.m2/settings.xml
-chown -R vagrant:vagrant /home/vagrant/.m2
+#mkdir -p /home/vagrant/.m2
+#cp /vagrant/scripts/settings.xml /home/vagrant/.m2/settings.xml
+#chown -R vagrant:vagrant /home/vagrant/.m2
